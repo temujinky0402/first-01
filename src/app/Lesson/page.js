1720de580c67fbf7d95e3a3253942eb0255fefd5
temujinky0402 <temujinky0402@gmail.com>
2026-05-13 13:59:45 +0800

    update

--- src/app/Lesson/page.js
+++ src/app/Lesson/page.js
@@ -17,14 +17,12 @@ export default function Lesson() {
                 <input value={nameSearch} onChange={(e) => setNameSearch(e.target.value)} placeholder="Welcome to Nest 11A" className="p-1.5 pl-4 ml-5 rounded-full border border-purple-700 text-white outline-none"></input>
             </div>
             
-            <div className="grid grid-cols-1 lg:grid-cols-5 gap-5 mb-50 mt-20 pr-15 pl-15">
+            <div className="grid grid-cols-1 gap-5">
                 {jsonData.filter((item) => item.First_name.toLowerCase().includes(nameSearch.toLowerCase())).map((item) => (
-                    (item.type == "student")
-                    ? 
-                    <div className="hover:scale-105 border-3 border-purple-700 rounded-xl bg-white cursor-pointer p-3 flex flex-col justify-between" key={item.id}>
+                     <div className="hover:scale-105 border-3 border-purple-700 rounded-xl bg-white cursor-pointer p-3 flex flex-col justify-between" key={item.id}>
                         <div>
                            <div className="flex flex-row justify-between">
-                                <p className="font-bold">{item.Last_name[0]}. {item.First_name}</p>
+                                <p className="font-bold">{item.Last_name}. {item.First_name}</p>
                                 <p className="text-purple-700 border-1 border-purple-700 rounded-xl p-2">{item.type}</p>
                             </div>
                             {(item.gender == "male") ? <img src="https://imagedelivery.net/LBWXYQ-XnKSYxbZ-NuYGqQ/7ff29767-f052-40ae-0571-20c89eab6400/banner" alt="" className="h-[35%] w-[35%]"></img> : <img src="https://i.redd.it/1y165vhkz1c51.png" alt="" className="h-[35%] w-[35%]"></img>}
@@ -35,22 +33,8 @@ export default function Lesson() {
                         
                         <button className="font-bold text-white bg-red-500 rounded-full p-2 mt-5 cursor-pointer">Details</button>
                     </div>
-                    :
-                    <div className="hover:scale-105 border-3 border-green-700 rounded-xl bg-white cursor-pointer p-3 flex flex-col justify-between" key={item.id}>
-                        <div>
-                            <div className="flex flex-row justify-between">
-                                <p className="font-bold">{item.Last_name[0]}. {item.First_name}</p>
-                                <p className="text-green-700 border-1 border-green-700 rounded-xl p-2">{item.type}</p>
-                            </div>
-                            <img src="https://tse2.mm.bing.net/th/id/OIP.dcyZasadquPyCfekD9g7jAHaFW?pid=Api&h=220&P=0" alt="" className="h-[35%] w-[35%]"></img>
-                            <p>Age: {item.age}</p>
-                            {(item.point > 59) ? <p className="text-green-500 font-bold">Grade: {item.point}</p> : <p className="text-red-500 font-bold"> Grade: {item.point}</p>}
-                            <p>Gender: {item.gender}</p>
-                        </div>
-
-                        <button className="font-bold text-white bg-red-500 rounded-full p-2 mt-5 cursor-pointer ">Details</button>
-                    </div>
-                ))}
+                ))
+                }
                 <div className="col-span-5 flex justify-center">
                     <button onClick={() => router.push("/")} className="text-white text-sm">click the logo to go to the homepage</button>
                 </div>
